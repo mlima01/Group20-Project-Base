e7029d5d4ccaae18497c178f5022415d455afefe
mlima <mlima@umd.edu>
2021-05-07 20:45:19 -0400

    Update script.js

--- public/createPlaylists/script.js
+++ public/createPlaylists/script.js
@@ -1,36 +1,19 @@
-// async function windowActions() {
-//     const artistInfoEndpoint = '/api/artist_info'
-//     const artistInfoRequest = await fetch(artistInfoEndpoint)
-//     const artistInfoData = await request.json()
-
-// const { format } = require("sequelize/types/lib/utils");
-
-//     const playlistDetailsEndpoint = '/api/playlist_details'
-//     const playlistDetailsRequest = await fetch(playlistDetailsEndpoint)
-//     const playlistDetailsData = await request.json()
-
-//     const songsEndpoint = '/api/songs'
-//     const songsRequest = await fetch(songsEndpoint)
-//     const songData = await request.json()
-
-    
-   
-function windowActions(){
-    async function createPlaylist(){
-        const form = document.querySelector('form')
-        const user = document.querySelector('#user')
-        const playlistName = document.querySelector('#namePlaylist')
-        const songs = document.querySelector('#songs')
-        const artist = document.querySelector('#followers')
-        const duration = document.querySelector('#songDuration')
-        console.log(duration)
-
-
+async function windowActions(){
+     
+        
+        const form = document.querySelector('#create')
+        const user = document.querySelector('#user').value
+        const playlistName = document.querySelector('#namePlaylist').value
+        const songs = document.querySelector('#songs').value
+        const artist = document.querySelector('#followers').value
+        const duration = document.querySelector('#totalTime').value
+        
         form.addEventListener('submit', async(event)=> {
             event.preventDefault();
             console.info('submitted form', event.target);
             
             
+                
             
             const post = await fetch('/api/playlists',
             {
@@ -39,22 +22,20 @@ function windowActions(){
                     'Content-Type':'application/json'
                 },
                 body: JSON.stringify({ 
-                    user_id: user.value,
-                    playlist_name: playlistName.value,
-                    number_of_songs: songs.value,
-                    number_of_followers: artist.value,
-                    total_time: duration.value,})
-            });
-            
-        
-        
-    })
-
-};
-
-
-createPlaylist();
+                    'user_id': user,
+                    'playlist_name': playlistName,
+                    'number_of_songs': songs,
+                    'number_of_followers': artist,
+                    'total_time': duration})
+            })
+        });
 }
+        
+    
+
+
+
+
    
 
 window.onload = windowActions; 
